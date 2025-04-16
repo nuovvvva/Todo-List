db94aef4a7ae7086f2061c1ae2cacd5aacc1f39a
nuovvvva <kanmengnuo@outlook.com>
2025-04-16 22:09:47 +0800

    Complete TodoInput component in App and apply it in TodoInput

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = useState([
     'Coding',
     'Coding and coding',
     'Coding day and night'
-  ];
+  ]);
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
 
   return (
     <>
-      <TodoInput />
+      <TodoInput handleAddTodos={handleAddTodos}/>
       <TodoList todos={todos}/>
     </>
 
